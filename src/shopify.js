8fe7a1fece9e0d023a54620ad7d8291d6decb8e8
ObtainedTeam <info@obtained.nl>
2026-06-30 13:16:54 +0200

    Update Shopify domain to bugawaygear.com

--- src/shopify.js
+++ src/shopify.js
@@ -1,4 +1,4 @@
-const DOMAIN = 'checkout.bugawaystore.com';
+const DOMAIN = 'checkout.bugawaygear.com';
 
 // Normalize color names between website and Shopify.
 // Website uses: 'Arctic White', 'Black'
