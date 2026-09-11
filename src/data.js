ac3991741f4605900503da937a0ba9d48f6664cc
ObtainedTeam <info@obtained.nl>
2026-09-11 16:36:15 +0200

    Add files via upload

--- src/data.js
+++ src/data.js
@@ -273,8 +273,8 @@ The tailored fit is designed specifically for women's proportions: a clean silho
     images: [
       "/images/Female _ White mesh _ mountain solo.png",
       "/images/Female _ White Mesh _ Front 1_1.jpg",
-      "/images/combo-lifestyle-couple-forest-trail.jpg",
-      "/images/jacket-women-lifestyle-garden-pruning.jpg",
+      "/images/combo-lifestyle-couple-forest-white.jpg",
+      "/images/jacket-women-lifestyle-gardening.jpg",
       "/images/detail-collage-white.jpg",
       ...STD,
     ],
